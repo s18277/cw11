d7f98fa58d5c84b230ef66affb1f4c84ce225b12
Paweł <73314096+s18277@users.noreply.github.com>
2021-01-12 20:42:11 +0100

    Add models for required tables

---
+++ Cw11/Models/PrescriptionMedicament.cs
@@ -0,0 +1,10 @@
+﻿namespace Cw11.Models
+{
+    public class PrescriptionMedicament
+    {
+        public int IdMedicament { get; set; }
+        public int IdPrescription { get; set; }
+        public int? Dose { get; set; }
+        public string Details { get; set; }
+    }
+}
