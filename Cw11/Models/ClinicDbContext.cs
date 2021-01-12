164bcab81807ca1b14c00c372c217da9b481baa3
Paweł <73314096+s18277@users.noreply.github.com>
2021-01-12 21:15:44 +0100

    Add entities configuration

--- Cw11/Models/ClinicDbContext.cs
+++ Cw11/Models/ClinicDbContext.cs
@@ -1,17 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Cw11.Configurations;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cw11.Models
 {
     public class ClinicDbContext : DbContext
     {
+        public ClinicDbContext() { }
+
+        public ClinicDbContext(DbContextOptions options) : base(options) { }
+
         public DbSet<Doctor> Doctors { get; set; }
         public DbSet<Patient> Patients { get; set; }
         public DbSet<Prescription> Prescriptions { get; set; }
         public DbSet<Medicament> Medicaments { get; set; }
         public DbSet<PrescriptionMedicament> PrescriptionMedicaments { get; set; }
 
-        public ClinicDbContext() { }
-
-        public ClinicDbContext(DbContextOptions options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new DoctorEfcConfiguration());
+            modelBuilder.ApplyConfiguration(new PatientEfcConfiguration());
+            modelBuilder.ApplyConfiguration(new PrescriptionEfcConfiguration());
+            modelBuilder.ApplyConfiguration(new MedicamentEfcConfiguration());
+            modelBuilder.ApplyConfiguration(new PrescriptionMedicamentEfcConfiguration());
+        }
     }
 }
