389fe025a633d819613309ae4d46df14021a024d
Paweł <73314096+s18277@users.noreply.github.com>
2021-01-12 20:50:44 +0100

    Add basic EFC DbContext

---
+++ Cw11/Models/ClinicDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cw11.Models
+{
+    public class ClinicDbContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Prescription> Prescriptions { get; set; }
+        public DbSet<Medicament> Medicaments { get; set; }
+        public DbSet<PrescriptionMedicament> PrescriptionMedicaments { get; set; }
+
+        public ClinicDbContext() { }
+
+        public ClinicDbContext(DbContextOptions options) : base(options) { }
+    }
+}
